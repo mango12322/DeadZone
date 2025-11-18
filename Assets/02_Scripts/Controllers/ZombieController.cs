6da8b4e7b0f32732f97ffc76df72be618a0f49b2
성현 <xpfl7920@gmail.com>
2025-11-19 06:13:56 +0900

    zombie

---
+++ Assets/02_Scripts/Controllers/ZombieController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    // 1. 타겟 지정
+    // 2. 탐지 거리
+    // 3. 공격 거리
+    // 4. 상태 애니메이션 적용
+    public Transform target;
+    public float detectRange = 10f;
+    public float attackRange = 2f;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
